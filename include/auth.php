0117df2ee00985f73ba9b71b383d71fc04f21c92
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-13 18:34:29 -0500

    Prepared a statement

--- include/auth.php
+++ include/auth.php
@@ -1,8 +1,15 @@
 <?php
 function validateCreds($username, $password) {
 	global $dispatchdb;
-	$query = "SELECT * FROM users WHERE username='$username'";
-	$result = $dispatchdb->query($query);
+
+	$query = $dispatchdb->prepare("SELECT * FROM users WHERE username=?");
+
+	$query->bind_param('s', $username)
+
+	$query->excecute();
+
+	$result = $query->get_result();
+	
 	$data = $result->fetch_array();
 
 	echo $data['username'];
@@ -13,6 +20,9 @@ function validateCreds($username, $password) {
 	$_SESSION['recordsClerk'] = $data['recordsClerk'];
 
 	echo "<!-- password1234 -->";
-	$result->free();
+	
+	$query->free_result();
+	$query->close();
+
 	return $data['password'] == md5($password);
 }
