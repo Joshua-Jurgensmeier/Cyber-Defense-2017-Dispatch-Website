dc91d10de262e12e447cea114f5d94a7ed049ae7
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-15 16:52:10 -0500

    bugs

--- accessControl.php
+++ accessControl.php
@@ -5,11 +5,26 @@ $officerList = ["/events.php", "/patrol/", "/patrol.php", "/upload"];
 $clerkList = ["/cars.php", "/patrol/", "/policeReport.php", "/policeReportNew.php", "/carNew.php"];
 $anyoneList = ["/login.php", "/logout.php", "/index.php", "/403.php"];
 
-if((!isset($_SESSION[])) or 
-	(!(($_SESSION['dispatcher'] and in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
-	($_SESSION['patrolOfficer'] and in_array($_SERVER['REQUEST_URI'], $officerList)) or 
-	($_SESSION['recordsClerk'] and in_array($_SERVER['REQUEST_URI'], $clerkList)) or
-	(in_array($_SERVER['REQUEST_URI'], $anyoneList)))))
+if(
+	in_array($_SERVER['REQUEST_URI'], $anyoneList) 
+
+	or 
+	
+	(
+		isset($_SESSION['authenticated']) 
+			
+			and 
+		( 
+			($_SESSION['dispatcher'] and in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
+			($_SESSION['patrolOfficer'] and in_array($_SERVER['REQUEST_URI'], $officerList)) or 
+			($_SESSION['recordsClerk'] and in_array($_SERVER['REQUEST_URI'], $clerkList))
+		)
+	)
+)
+
+{}
+
+else 
 {
 	header('Location: http://www.team12.isucdc.com/403.php');
 	exit("Access denied");
