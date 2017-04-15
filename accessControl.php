6f3b3fe83d57d88e6e198fa9755911441048cd35
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-15 15:51:31 -0500

    bug

--- accessControl.php
+++ accessControl.php
@@ -5,11 +5,10 @@ $officerList = ["/events.php", "/patrol/", "/patrol.php", "/upload"];
 $clerkList = ["/cars.php", "/patrol/", "/policeReport.php", "/policeReportNew.php", "/carNew.php"];
 $anyoneList = ["/login.php", "/logout.php", "/index.php", "/403.php"];
 
-if((!isset($_SESSION['dispatcher'])) or 
-	(!(($_SESSION['dispatcher'] and $in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
+if(!(($_SESSION['dispatcher'] and $in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
 	($_SESSION['patrolOfficer'] and $in_array($_SERVER['REQUEST_URI'], $officerList)) or 
 	($_SESSION['recordsClerk'] and $in_array($_SERVER['REQUEST_URI'], $clerkList)) or
-	($in_array($_SERVER['REQUEST_URI'], $anyoneList)))))
+	($in_array($_SERVER['REQUEST_URI'], $anyoneList))))
 {
 	header('Location: http://www.team12.isucdc.com/403.php');
 	exit("Access denied");
