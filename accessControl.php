5ab6d02a3688cb23da5099da44765f8022a2f258
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-15 16:15:27 -0500

    bug

--- accessControl.php
+++ accessControl.php
@@ -5,9 +5,9 @@ $officerList = ["/events.php", "/patrol/", "/patrol.php", "/upload"];
 $clerkList = ["/cars.php", "/patrol/", "/policeReport.php", "/policeReportNew.php", "/carNew.php"];
 $anyoneList = ["/login.php", "/logout.php", "/index.php", "/403.php"];
 
-if(!(($_SESSION['dispatcher'] and $in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
-	 ($_SESSION['patrolOfficer'] and $in_array($_SERVER['REQUEST_URI'], $officerList)) or 
-	 ($_SESSION['recordsClerk'] and $in_array($_SERVER['REQUEST_URI'], $clerkList)) or
+if(!(($_SESSION['dispatcher'] and in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
+	 ($_SESSION['patrolOfficer'] and in_array($_SERVER['REQUEST_URI'], $officerList)) or 
+	 ($_SESSION['recordsClerk'] and in_array($_SERVER['REQUEST_URI'], $clerkList)) or
 	 in_array($_SERVER['REQUEST_URI'], $anyoneList)))
 {
 	header('Location: http://www.team12.isucdc.com/403.php');
