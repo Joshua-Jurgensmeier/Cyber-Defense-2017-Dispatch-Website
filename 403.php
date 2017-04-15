09f2bf999eb15f7bd6197f2443acafef0540eb02
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-15 16:25:13 -0500

    bogs

--- 403.php
+++ 403.php
@@ -1,8 +1,15 @@
-<?php include("head.php"); ?>
+<?php include "head.php"; 
+
+echo in_array($_SERVER['REQUEST_URI'], $anyoneList);
+echo isset($_SESSION[]);
+
+?>
 <h2>403: Access Denied</h2>
+<h2>A warrant has been issued for your arrest.</h2>
 <div style='width:100%; text-align: center;'>
 	<img alt="Police - Iowa State University" src='ISU_PD_LOGO.jpg'/><br/>
 	<h2>Iowa State University Police</h2>
-	<h2>A warrant has been issued for your arrest</h2>
+
+
 </div>
 <?php include("foot.php"); ?>
--- accessControl.php
+++ accessControl.php
@@ -5,10 +5,11 @@ $officerList = ["/events.php", "/patrol/", "/patrol.php", "/upload"];
 $clerkList = ["/cars.php", "/patrol/", "/policeReport.php", "/policeReportNew.php", "/carNew.php"];
 $anyoneList = ["/login.php", "/logout.php", "/index.php", "/403.php"];
 
-if(!(($_SESSION['dispatcher'] and in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
-	 ($_SESSION['patrolOfficer'] and in_array($_SERVER['REQUEST_URI'], $officerList)) or 
-	 ($_SESSION['recordsClerk'] and in_array($_SERVER['REQUEST_URI'], $clerkList)) or
-	 in_array($_SERVER['REQUEST_URI'], $anyoneList)))
+if((!isset($_SESSION['dispatcher'])) or 
+	(!(($_SESSION['dispatcher'] and $in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
+	($_SESSION['patrolOfficer'] and $in_array($_SERVER['REQUEST_URI'], $officerList)) or 
+	($_SESSION['recordsClerk'] and $in_array($_SERVER['REQUEST_URI'], $clerkList)) or
+	($in_array($_SERVER['REQUEST_URI'], $anyoneList)))))
 {
 	header('Location: http://www.team12.isucdc.com/403.php');
 	exit("Access denied");
