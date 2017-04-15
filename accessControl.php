346d0828260197b8335959081af2afccc1da22da
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-15 16:00:52 -0500

    bug

--- accessControl.php
+++ accessControl.php
@@ -6,9 +6,9 @@ $clerkList = ["/cars.php", "/patrol/", "/policeReport.php", "/policeReportNew.ph
 $anyoneList = ["/login.php", "/logout.php", "/index.php", "/403.php"];
 
 if(!(($_SESSION['dispatcher'] and $in_array($_SERVER['REQUEST_URI'], $dispatcherList)) or 
-	($_SESSION['patrolOfficer'] and $in_array($_SERVER['REQUEST_URI'], $officerList)) or 
-	($_SESSION['recordsClerk'] and $in_array($_SERVER['REQUEST_URI'], $clerkList)) or
-	($in_array($_SERVER['REQUEST_URI'], $anyoneList))))
+	 ($_SESSION['patrolOfficer'] and $in_array($_SERVER['REQUEST_URI'], $officerList)) or 
+	 ($_SESSION['recordsClerk'] and $in_array($_SERVER['REQUEST_URI'], $clerkList)) or
+	 in_array($_SERVER['REQUEST_URI'], $anyoneList)))
 {
 	header('Location: http://www.team12.isucdc.com/403.php');
 	exit("Access denied");
